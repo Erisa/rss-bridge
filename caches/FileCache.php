21f4ccdb017754af0bec94473f009d0bd788ca3d
Erisa A <erisa@erisa.uk>
2022-12-15 19:48:11 +0000

    remove concat

--- caches/FileCache.php
+++ caches/FileCache.php
@@ -98,7 +98,7 @@ class FileCache implements CacheInterface
 
         if (!is_dir($this->path)) {
             if (mkdir($this->path, 0755, true) !== true) {
-                throw new \Exception('mkdir: Unable to create file cache folder' . ': ' . path);
+                throw new \Exception("mkdir: Unable to create file cache folder: $path");
             }
         }
 
